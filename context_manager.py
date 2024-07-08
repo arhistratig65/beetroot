6b32eb4797d9d4e831244fea31ad51dfb3deedde
arhistratig65 <arhistratig65@gmail.com>
2024-07-08 15:55:21 +0300

    00

---
+++ context_manager.py
@@ -0,0 +1,19 @@
+import logging
+
+
+class ContextManager:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        self.file = open(self.filename, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type == IndexError:
+            logging.error(f'IndexError occurred: {exc_value}')
+            return True  # Позначте виняток як оброблений
+        else:
+            self.file.close()
+            # Дозволити розповсюдження винятків, відмінних від IndexError
+            return False
